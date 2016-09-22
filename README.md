da862a28826c5651364cc6ff0c9c0f1fab1c1d8a
Cam Jackson <camjackson89@gmail.com>
2016-09-23 09:21:57 +1000

    Paging through an Event Store stream using async/await magic

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Redux middleware for reading and writing actions to Event Store
 
 Under construction, obviously :)
+
+Docker commands:
+
+```sh
+docker pull eventstore/eventstore
+docker run --name eventstore-node -it -p 2113:2113 -p 1113:1113 eventstore/eventstore
+```
--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('Hello, world!');
+'use strict';
+const http = require('http');
+const url = require('url');
+
+const headers = { Accept: 'application/json' };
+
+const getLink = (payload, linkName) => payload.links.find(uri => uri.relation === linkName).uri;
+
+const get = uri => (
+  new Promise(resolve => (
+    http.get({ ...url.parse(uri), headers }, res => {
+      let body = '';
+      res.on('data', data => { body += data; });
+      res.on('end', () => { resolve(JSON.parse(body)); });
+      //TODO: reject on errors
+    })
+  ))
+);
+
+let events = [];
+
+async function main() {
+  const stream = await get('http://0.0.0.0:2113/streams/newstream');
+  let nextUri = getLink(stream, 'last');
+  let page;
+
+  do {
+    page = await get(nextUri);
+    events = events.concat(page.entries.reverse());
+    nextUri = getLink(page, 'previous');
+  } while(!page.headOfStream);
+
+  console.log(events);
+}
+
+main();
